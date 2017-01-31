eeff11cad6f9df84b54bbb0187cc6070472267dd
Glynnis Thomas <glynnisthomas@gmail.com>
2017-01-31 01:18:06 -0600

    Added client and server side validation, Still needs string replacement and escaping

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 import cgi
+import re
 
 #html boilerplate for the top of the page
 header = '''
@@ -23,6 +24,7 @@ header = '''
     <html lang='en'>
         <head>
             <meta charset="utf-8">
+            <link type="text/css" rel="stylesheet" href="stylesheet.css" />
             <title>User Signup</title>
         </head>
         <body>
@@ -36,50 +38,111 @@ footer = '''
 '''
 
 #form data
-form = '''
-    <form action="/thankyou" method="post">
-        <label>Username:
-            <input type="text" name="username" />
-        </label>
-        <br>
-        <label>Password:
-            <input type="password" name="password" />
-        </label>
-        <br>
-        <label>Confirm Password:
-            <input type="password" name="password2" />
-        </label>
-        <br>
-        <label>Email: (optional)
-            <input type="email" name="email" />
-        </label>
+form_main = '''
+    <form method="post">
+        <table>
+        <tbody>
+            <tr>
+                <td><label>Username:</label></td>
+                <td><input type="text" name="username"  pattern="^[a-zA-Z0-9_-]{3,20}$" value="" required />
+
+                </td>
+            </tr>
+            <tr>
+                <td><label>Password:</label></td>
+                <td><input type="password" name="password" pattern="^.{3,20}$" required />
+
+                </td>
+            </tr>
+            <tr>
+                <td><label>Confirm Password:</label></td>
+                <td><input type="password" name="password2" required />
+
+            </tr>
+            <tr>
+                <td><label>Email: (optional)</label></td>
+                <td><input type="email" name="email" value="" />
+
+            </tr>
+        </tbody>
+        </table>
         <br>
         <input type="submit" />
     </form>
 '''
 
-'''
-Need to: make function to verify each feild, create post function for Handler, Create thank you page and handler,
-'''
-
-def verify_username(username):
-    pass
-
-def verify_password(password):
-    pass
-
 
 class MainHandler(webapp2.RequestHandler):
+    def verify_username(self, username):
+        un = self.request.get("username")
+        temp = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+        username = temp.match(un)
+        if username == True:
+            return True
+        else:
+            return False
+
+    def verify_password(self, password):
+        pw = self.request.get("password")
+        temp = re.compile("^.{3,20}$")
+        password = temp.match(pw)
+        if password == True:
+            return True
+        else:
+            return False
+
+    def valid_email(self, email):
+        em = self.request.get("email")
+        temp = re.compile("^[\S]+@[\S]+.[\S]+$")
+        email = temp.match(em)
+        if email == True:
+            return True
+        else:
+            return False
+
+    # def create_form(self, username="", email=""):
+    #     self.response.write(form_main % {"username": username, "email": email})
+
     def get(self):
-        
-        self.response.write(header + form + footer)
+        error = self.request.get("error")
+        error_element = "<p class='error'>" + error + "</p>" if error else ""
+        #form = self.create_form()
+        self.response.write(header + form_main + error_element + footer)
+
+    def post(self):
+        username = self.request.get("username")
+        pass1 = self.request.get("password")
+        pass2 = self.request.get("password2")
+        email = self.request.get("email")
+
+        if not username or (self.verify_username(username) == False):
+            error = "Please enter a valid username."
+            self.redirect('/?error=' + error)
+
+        if (pass1 != pass2) or (pass1 == '') or (pass2 == ''):
+            error = "Your passwords do not match. Please enter matching passwords."
+            self.redirect('/?error=' + error)
+
+        if pass1 == '' and pass2 == '':
+            error = "Please enter a valid password."
+            self.redirect('/?error=' + error)
+
+        if email and self.valid_email(email) == False:
+            error = "Please enter a valid email."
+            self.redirect('/?error=' + error)
+
+        #form = self.create_form(username, email)
+        content = header + form_main + footer
+        self.response.write(content)
+
 
 class ThankYouHandler(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         username = self.request.get("username")
-        self.response.write("<h2>Thank you " + username + "!</h2>")
+        self.response.write("<h2>Welcome, " + username + "!</h2>")
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/thankyou', ThankYouHandler)
 ], debug=True)
